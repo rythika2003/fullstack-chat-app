2c4e023505475d183508a96c6d13e2252fc9feb9
Rythika <rythikapriya@gmail.com>
2025-05-28 15:51:25 +0530

    Project completed

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,16 +1,11 @@
 // backend/src/routes/user.routes.js
 import express from 'express';
 const router = express.Router();
-import express from "express";
 import { match, pathToRegexp, compile, parse, stringify } from "path-to-regexp";
-
-// Now you can use those functions below
-
 
 const route = "/user/:id";
 const keys = [];
 const regexp = pathToRegexp(route, keys);
-
 
 router.get("/", (req, res) => {
   res.send("User route works!");
