7391e8fd070b65da45e4122258313ecf808b1c20
Rythika <rythikapriya@gmail.com>
2025-05-28 15:59:35 +0530

    Project completed

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,11 +1,6 @@
 // backend/src/routes/user.routes.js
 import express from 'express';
 const router = express.Router();
-import { match, pathToRegexp, compile, parse, stringify } from "path-to-regexp";
-
-const route = "/user/:id";
-const keys = [];
-const regexp = pathToRegexp(route, keys);
 
 router.get("/", (req, res) => {
   res.send("User route works!");
